acf3b1ff2b960ea0d92bfd87ce43010a0301df4e
Patrick Riley <priley@relaytx.com>
2024-01-01 17:18:15 -0500

    Add actions

--- pennydropsolver/__init__.py
+++ pennydropsolver/__init__.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from enum import Enum
 
 import numpy as np
 
@@ -8,7 +9,13 @@ def bits_to_encode(n):
 
 
 # The terminal sate will be all -1
-GameState = namedtuple("GameStats", ["num_out", "player", "is_first"])
+GameState = namedtuple("GameState", ["num_out", "player", "is_first"])
+ActionResult = namedtuple("ActionResult", ["prob", "reward", "next_state"])
+
+
+class Action(Enum):
+    ROLL = 0
+    PASS = 1
 
 
 class GameModel:
@@ -59,3 +66,35 @@ class GameModel:
         state_idx >>= self._num_players_bits
         num_out = state_idx
         return GameState(num_out=num_out, player=player, is_first=is_first)
+
+    def allowed_actions(self, state):
+        if state.is_first:
+            return [Action.ROLL]
+        return [Action.ROLL, Action.PASS]
+
+    def do_action(self, state, act):
+        if act == Action.PASS:
+            yield ActionResult(
+                prob=1.0,
+                reward=0,
+                next_state=GameState(
+                    num_out=state.num_out,
+                    player=(state.player + 1) % self.num_players,
+                    is_first=True,
+                ),
+            )
+            return
+        prob_loss = state.num_out / self.num_spots
+        if state.num_out != 0:
+            yield ActionResult(
+                prob=prob_loss,
+                reward=state.num_out if state.player == 0 else 0,
+                next_state=GameState(num_out=-1, player=-1, is_first=False),
+            )
+        yield ActionResult(
+            prob=1.0 - prob_loss,
+            reward=-1 if state.player == 0 else 0,
+            next_state=GameState(
+                num_out=state.num_out + 1, player=state.player, is_first=False
+            ),
+        )
