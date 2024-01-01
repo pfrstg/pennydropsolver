81c703a8117b3578474047e0a42b09c205eeb39c
Patrick Riley <priley@relaytx.com>
2024-01-01 09:39:33 -0500

    First bit of code

--- pennydropsolver/__init__.py
+++ pennydropsolver/__init__.py
@@ -1,1 +1,11 @@
+from collections import namedtuple
+
 import numpy
+
+GameState = namedtuple("GameStats", ["num_out", "player", "is_first"])
+
+
+class GameModel:
+    def __init__(self, num_players, num_spots):
+        self.num_players = num_players
+        self.num_spots = num_spots
---
+++ tests/test_all.py
@@ -0,0 +1,5 @@
+import pennydropsolver
+
+
+def test_simple():
+    model = pennydropsolver.GameModel(num_players=2, num_spots=6)
