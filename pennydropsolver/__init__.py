412c9c0882f305f6def5d24bd9b9fd2a4b3fdb79
Patrick Riley <priley@relaytx.com>
2024-01-01 15:23:01 -0500

    State indexing

--- pennydropsolver/__init__.py
+++ pennydropsolver/__init__.py
@@ -1,7 +1,13 @@
 from collections import namedtuple
 
-import numpy
+import numpy as np
 
+
+def bits_to_encode(n):
+    return 1 + int(np.log2(n - 1))
+
+
+# The terminal sate will be all -1
 GameState = namedtuple("GameStats", ["num_out", "player", "is_first"])
 
 
@@ -9,3 +15,47 @@ class GameModel:
     def __init__(self, num_players, num_spots):
         self.num_players = num_players
         self.num_spots = num_spots
+
+        self._num_players_bits = bits_to_encode(self.num_players)
+        self._num_spots_bits = bits_to_encode(self.num_spots)
+
+    def num_states(self):
+        return 1 + self.num_players * self.num_spots * 2
+
+    def max_state_index(self):
+        return 1 + (1 << self._num_players_bits) * (1 << self._num_spots_bits) * 2
+
+    def is_terminal_state(self, state):
+        return state.num_out == -1 and state.player == -1
+
+    def is_state_valid(self, state):
+        if self.is_terminal_state(state):
+            return True
+        return (
+            state.num_out >= 0
+            and state.num_out < self.num_spots
+            and state.player >= 0
+            and state.player < self.num_players
+            and (state.is_first == True or state.is_first == False)
+        )
+
+    def state_to_state_index(self, state):
+        if self.is_terminal_state(state):
+            return 0
+        return (
+            1
+            + (state.num_out << (self._num_players_bits + 1))
+            + (state.player << 1)
+            + (0 if state.is_first else 1)
+        )
+
+    def state_index_to_state(self, state_idx):
+        if state_idx == 0:
+            return GameState(-1, -1, True)
+        state_idx -= 1
+        is_first = False if (1 & state_idx) else True
+        state_idx >>= 1
+        player = state_idx & ~(~0 << self._num_players_bits)
+        state_idx >>= self._num_players_bits
+        num_out = state_idx
+        return GameState(num_out=num_out, player=player, is_first=is_first)
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,4 +1,44 @@
+import pytest
+
 import pennydropsolver
+
+
+@pytest.mark.parametrize(
+    "n,expected", [(2, 1), (3, 2), (4, 2), (5, 3), (7, 3), (8, 3), (9, 4)]
+)
+def test_bits_to_encode(n, expected):
+    assert pennydropsolver.bits_to_encode(n) == expected
+
+
+@pytest.mark.parametrize(
+    "num_players,num_spots,expected_states,expected_max",
+    [
+        (2, 6, 25, 33),
+        (3, 2, 13, 17),
+    ],
+)
+def test_state_counts(num_players, num_spots, expected_states, expected_max):
+    model = pennydropsolver.GameModel(num_players=num_players, num_spots=num_spots)
+    assert model.num_states() == expected_states
+    assert model.max_state_index() == expected_max
+
+
+@pytest.mark.parametrize(
+    "num_players,num_spots",
+    [
+        (2, 6),
+        (3, 2),
+        (4, 7),
+    ],
+)
+def test_total_states(num_players, num_spots):
+    model = pennydropsolver.GameModel(num_players=num_players, num_spots=num_spots)
+    total_valid = 0
+    for state_idx in range(model.max_state_index()):
+        state = model.state_index_to_state(state_idx)
+        if model.is_state_valid(state):
+            total_valid += 1
+            assert model.state_to_state_index(state) == state_idx, state
 
 
 def test_simple():
